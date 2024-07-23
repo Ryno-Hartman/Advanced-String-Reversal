f4f486e69e416e04f76cb02f0d081fe68ed561d3
Ryno-Hartman <rynohartman16@gmail.com>
2024-07-23 13:15:55 +0200

    Add files via upload

---
+++ StringService/StringService/Controller.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ReverseStringAPI.Services;
+using System.Runtime.InteropServices;
+
+namespace ReverseStringAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StringController : ControllerBase
+    {
+        private readonly StringService _stringService;
+
+        public StringController()
+        {
+            _stringService = new StringService();
+        }
+
+        [HttpGet("process")]
+        public IActionResult ProcessString([FromQuery] string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return BadRequest("Input string cannot be null or empty.");
+            }
+
+            var reversed = _stringService.ReverseString(input);
+            var isPalindrome = _stringService.IsPalindrome(input);
+
+            var response = new
+            {
+                ReversedString = reversed,
+                IsPalindrome = isPalindrome
+            };
+
+            return Ok(response);
+        }
+    }
+}
---
+++ StringService/StringService/StringService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ReverseStringAPI.Services
+{
+    public class StringService
+    {
+        public string ReverseString(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return input;
+
+            char[] charArray = input.ToCharArray();
+            Array.Reverse(charArray);
+            return new string(charArray);
+        }
+
+        public bool IsPalindrome(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+                return false;
+
+            string reversed = ReverseString(input);
+            return string.Equals(input, reversed, StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
